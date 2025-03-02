402d93c82a902d77ce58deda39d66bade172082a
DimaSh888 <dimon.shipicin@gmail.com>
2025-03-02 17:22:48 +0200

    Update Project.py
    перс + рисовка перса

--- Project.py
+++ Project.py
@@ -23,6 +23,8 @@ window=display.set_mode((800,600))
 display.set_caption("Магічний котел")
 background=transform.scale(image.load("background.jpg"),(800,600))
 
+magician=Player("magician.png",50,500,100,100,10)
+
 game=True
 clock=time.Clock()
 FPS=60
@@ -39,7 +41,10 @@ while game:
     for e in event.get():
         if e.type==QUIT:
             game=False
+            
     window.blit(background,(0,0))
+    magician.reset()
+    magician.update()  
 
     display.update()
     time.delay(50)
