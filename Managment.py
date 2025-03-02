2da09746dec017a3e552a5ebe383b1273f498101
gr1vnia <149868287+gr1vnia@users.noreply.github.com>
2025-03-02 17:07:34 +0200

    Managment.py

---
+++ Managment.py
@@ -0,0 +1,19 @@
+class Player():
+    def Update(self):
+        Keys = key.get_pressed()
+        if Keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if Keys[K_d] and self.rect.x < 650:
+            self.rect.x += self.speed
+        if Keys[K_w] and self.rect.x > 5:
+            self.rect.y -= self.speed
+        if Keys[K_s] and self.rect.x < 450:
+            self.rect.y += self.speed
+
+while Managment:
+    for e in event.get():
+        if e.type == QUIT:
+            Game = False
+        elif: e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                Player.fire()
