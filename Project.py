fd60f4eacb57d5bf6a5b08b665efeadc9915f161
DimaSh888 <dimon.shipicin@gmail.com>
2025-03-02 16:59:59 +0200

    Update Project.py
    фон + вікно + ігровий цикл + підулючення модулю pygame

--- Project.py
+++ Project.py
@@ -1,1 +1,19 @@
+from pygame import * 
 
+window=display.set_mode((800,600))
+display.set_caption("Магічний котел")
+background=transform.scale(image.load("background.jpg"),(800,600))
+
+game=True
+clock=time.Clock()
+FPS=60
+
+while game:
+    for e in event.get():
+        if e.type==QUIT:
+            game=False
+    window.blit(background,(0,0))
+
+    display.update()
+    time.delay(50)
+    clock.tick(FPS)
