fd859153ff3205f4169410707ce4af3dd6cefd6a
amberrin <73347974+amberrin@users.noreply.github.com>
2025-03-09 17:24:55 +0200

    Update Project.py
    classes

--- Project.py
+++ Project.py
@@ -1,13 +1,14 @@
 from pygame import * 
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y,size_x,size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, player_jump):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (size_x,size_y))
+        self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+        self.jump = player_jump
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -16,7 +17,19 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x <750:
+        if keys[K_RIGHT] and self.rect.x <650:
+            self.rect.x += self.speed
+
+class Enemy(GameSprite):
+    direction = "left"
+    def update(self):
+        if self.rect.x<=454:
+            self.direction = "right"
+        if self.rect.x >= 725:
+            self.direction = "left"
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        else:
             self.rect.x += self.speed
 
 window=display.set_mode((800,600))
@@ -87,7 +100,13 @@ while game:
     for e in event.get():
         if e.type==QUIT:
             game=False
-            
+    magician.rect.y += magician.speed
+    if sprite.spritecollide(magician,platforms, False):
+         magician.rect.y -= magician.speed
+    keys = key.get_pressed()
+    if keys[K_UP] and magician.rect.y > 20 and sprite.spritecollide(magician,platforms, False):
+        magician.rect.y -= magician.jump
+    
     window.blit(background,(0,0))
     magician.reset()
     magician.update()  
